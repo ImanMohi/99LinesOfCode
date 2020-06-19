10b3e120a26d3c7e85d0751d2e888192bfd07de0
Iman Mohi <imanmohi786@gmail.com>
2020-06-19 16:07:40 +0530

    updating js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let friends =  ["Bob", "Carl", "Matt", "David", "James"];
 
+for (friend of friends)
+{
+    console.log(friend+": ");
+    for (let i=99; i>0; i--)
+    {
+        console.log(i+" lines of code, "+ i+ " lines of code;    " + friend + " strikes one out! "+ (i-1)+ " lines of code left..");
+    }
+}
